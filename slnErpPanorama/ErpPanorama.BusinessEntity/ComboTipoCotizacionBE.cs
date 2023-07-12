b64c5d8f18a9928cdc6f90f33f38065fb875abd6
programador <panorama.sistemas2023@gmail.com>
2023-07-12 12:53:26 -0500

    Se crea  la clase ComboTipoCotizacionBE con los atributos [DataContract] y [DataMember], considerando el namespace ErpPanorama.BusinessEntity y la estructura de la tabla TablaElemento

---
+++ slnErpPanorama/ErpPanorama.BusinessEntity/ComboTipoCotizacionBE.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace ErpPanorama.BusinessEntity
+{
+    [DataContract]
+    public class ComboTipoCotizacionBE
+    {
+        [DataMember]
+        public int IdTablaElemento { get; set; }
+
+        [DataMember]
+        public int? IdTabla { get; set; }
+
+        [DataMember]
+        public string Abreviatura { get; set; }
+
+        [DataMember]
+        public string DescTablaElemento { get; set; }
+
+        [DataMember]
+        public int? IdTablaExterna { get; set; }
+
+        [DataMember]
+        public decimal? Valor { get; set; }
+
+        [DataMember]
+        public bool? FlagEstado { get; set; }
+    }
+}
