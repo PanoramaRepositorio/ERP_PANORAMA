d6b3cdcd68eaf0ad4621f0a4ca7b72fa9428e85d
programador <panorama.sistemas2023@gmail.com>
2023-07-18 09:43:39 -0500

    se crea nuevo formulario principal para llamar al formulario cotización

---
+++ slnErpPanorama/ErpPanorama.Presentation/Modulos/KiraHogar/Registros/frmRegKiraCotizacion.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ErpPanorama.Presentation.Modulos.KiraHogar.Registros
+{
+    partial class frmRegKiraCotizacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlbMenu = new ErpPanorama.Presentation.ControlUser.UIToolBar();
+            this.SuspendLayout();
+            // 
+            // tlbMenu
+            // 
+            this.tlbMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tlbMenu.Ensamblado = "";
+            this.tlbMenu.Location = new System.Drawing.Point(0, 0);
+            this.tlbMenu.Name = "tlbMenu";
+            this.tlbMenu.Size = new System.Drawing.Size(1742, 31);
+            this.tlbMenu.TabIndex = 0;
+            this.tlbMenu.NewClick += new ErpPanorama.Presentation.ControlUser.UIToolBar.delegateNewClick(this.tlbMenu_NewClick);
+            this.tlbMenu.Load += new System.EventHandler(this.tlbMenu_Load);
+            // 
+            // frmRegKiraCotizacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1742, 557);
+            this.Controls.Add(this.tlbMenu);
+            this.Name = "frmRegKiraCotizacion";
+            this.Text = "frmRegKiraCotizacion";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmRegKiraCotizacion_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ControlUser.UIToolBar tlbMenu;
+    }
+}
---
+++ slnErpPanorama/ErpPanorama.Presentation/Modulos/KiraHogar/Registros/frmRegKiraCotizacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ErpPanorama.Presentation.Modulos.KiraHogar.Consultas;
+
+namespace ErpPanorama.Presentation.Modulos.KiraHogar.Registros
+{
+    public partial class frmRegKiraCotizacion : DevExpress.XtraEditors.XtraForm
+    {
+        public frmRegKiraCotizacion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRegKiraCotizacion_Load(object sender, EventArgs e)
+        {
+            frmRegKiraCotizacion formCotizacion = new frmRegKiraCotizacion();
+            formCotizacion.WindowState = FormWindowState.Maximized;
+            tlbMenu.Ensamblado = this.Tag.ToString();
+          
+        }
+
+        private void tlbMenu_NewClick()
+        {
+           
+            frmCotizacion formCotizacion = new frmCotizacion();
+            formCotizacion.Dock = DockStyle.Fill; // Rellenar el área del contenedor
+            formCotizacion.StartPosition = FormStartPosition.CenterParent;
+            formCotizacion.Show(); // Mostrar el formulario
+
+
+        }
+
+        private void tlbMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
