023bb153b4b5cbaf7b6e2639088e7df917273499
programador <panorama.sistemas2023@gmail.com>
2023-07-19 22:08:56 -0500

    ok

---
+++ slnErpPanorama/BusinessLogic/CotizacionKiraBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ErpPanorama.BusinessEntity;
+using ErpPanorama.DataLogic;
+
+namespace ErpPanorama.BusinessLogic
+{
+
+    public class CotizacionKiraBL
+    {
+        private CotizacionKiraDL cotizacionKiraDL;
+
+        public CotizacionKiraBL()
+        {
+            cotizacionKiraDL = new CotizacionKiraDL();
+        }
+
+        public void RegistrarCotizacionYDetalle(CotizacionKiraBE cotizacion, List<DetalleCotizacionBE> detallesCotizacion)
+        {
+            cotizacionKiraDL.RegistrarCotizacionYDetalle(cotizacion, detallesCotizacion);
+        }
+    }
+}
