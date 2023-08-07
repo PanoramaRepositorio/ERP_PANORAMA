0b62dd901872b85bf263c951c9c930169cf71b9b
programador <panorama.sistemas2023@gmail.com>
2023-08-07 13:06:04 -0500

    se creo nuevo formululario para selecionar

---
+++ slnErpPanorama/ErpPanorama.Presentation/Modulos/KiraHogar/Consultas/frmSeleccionarFormulario.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ErpPanorama.Presentation.Modulos.KiraHogar.Consultas
+{
+    partial class frmSeleccionarFormulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrirCotizacion = new System.Windows.Forms.Button();
+            this.btnAbrirProductoTerminado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrirCotizacion
+            // 
+            this.btnAbrirCotizacion.Location = new System.Drawing.Point(12, 12);
+            this.btnAbrirCotizacion.Name = "btnAbrirCotizacion";
+            this.btnAbrirCotizacion.Size = new System.Drawing.Size(171, 23);
+            this.btnAbrirCotizacion.TabIndex = 0;
+            this.btnAbrirCotizacion.Text = "Precio Producto Cliente Stock";
+            this.btnAbrirCotizacion.UseVisualStyleBackColor = true;
+            this.btnAbrirCotizacion.Click += new System.EventHandler(this.btnAbrirCotizacion_Click);
+            // 
+            // btnAbrirProductoTerminado
+            // 
+            this.btnAbrirProductoTerminado.Location = new System.Drawing.Point(189, 12);
+            this.btnAbrirProductoTerminado.Name = "btnAbrirProductoTerminado";
+            this.btnAbrirProductoTerminado.Size = new System.Drawing.Size(162, 23);
+            this.btnAbrirProductoTerminado.TabIndex = 1;
+            this.btnAbrirProductoTerminado.Text = "Precio Producto Terminado";
+            this.btnAbrirProductoTerminado.UseVisualStyleBackColor = true;
+            this.btnAbrirProductoTerminado.Click += new System.EventHandler(this.btnAbrirProductoTerminado_Click);
+            // 
+            // frmSeleccionarFormulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 46);
+            this.Controls.Add(this.btnAbrirProductoTerminado);
+            this.Controls.Add(this.btnAbrirCotizacion);
+            this.Name = "frmSeleccionarFormulario";
+            this.Text = "Escoja registro";
+            this.Load += new System.EventHandler(this.frmSeleccionarFormulario_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrirCotizacion;
+        private System.Windows.Forms.Button btnAbrirProductoTerminado;
+    }
+}
---
+++ slnErpPanorama/ErpPanorama.Presentation/Modulos/KiraHogar/Consultas/frmSeleccionarFormulario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ErpPanorama.Presentation.Modulos.KiraHogar.Registros;
+
+namespace ErpPanorama.Presentation.Modulos.KiraHogar.Consultas
+{
+    public partial class frmSeleccionarFormulario : Form
+    {
+        public frmSeleccionarFormulario()
+        {
+            InitializeComponent();
+        }
+
+        // Agregar una propiedad para almacenar la selección del formulario
+        public string FormularioSeleccionado { get; private set; }
+
+        private void frmSeleccionarFormulario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrirCotizacion_Click(object sender, EventArgs e)
+        {
+            FormularioSeleccionado = "Cotizacion";
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnAbrirProductoTerminado_Click(object sender, EventArgs e)
+        {
+            FormularioSeleccionado = "ProductoTerminado";
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
