8bd90adac9ace3225658a6c44110c6a7ff521878
PanoramaRepositorio <138617152+PanoramaRepositorio@users.noreply.github.com>
2023-10-11 17:28:35 -0500

    se agrego metodos a la capa negocio - inserta , inserta dt , Actualiza , ListaTodosActivo , Actualizadt, Elimina,Eliminadt, Eliminatododt, Selecciona.

--- slnErpPanorama/BusinessLogic/PromocionVolumenBL.cs
+++ slnErpPanorama/BusinessLogic/PromocionVolumenBL.cs
@@ -32,28 +32,28 @@ namespace ErpPanorama.BusinessLogic
         }
 
 
-        public void Inserta(PromocionTemporalBE pItem, List<PromocionTemporalDetalleBE> pListaPromocionTemporalDetalle)
+        public void Inserta(PromocionVolumenBE pItem, List<PromocionVolumenDetalleBE> pListaPromocionVolumenlDetalle)
         {
             try
             {
                 using (TransactionScope ts = new TransactionScope())
                 {
-                    PromocionTemporalDL PromocionTemporal = new PromocionTemporalDL();
-                    PromocionTemporalDetalleDL PromocionTemporalDetalle = new PromocionTemporalDetalleDL();
+                    PromocionVolumenDL PromocionVolumen = new PromocionVolumenDL();
+                    PromocionVolumenDetalleDL PromocionVolumenlDetalle = new PromocionVolumenDetalleDL();
 
-                    int IdPromocionTemporal = 0;
-                    IdPromocionTemporal = PromocionTemporal.Inserta(pItem);
+                    int IdPromocionVolumen = 0;
+                    IdPromocionVolumen = PromocionVolumen.Inserta(pItem);
 
-                    foreach (PromocionTemporalDetalleBE item in pListaPromocionTemporalDetalle)
+                    foreach (PromocionVolumenDetalleBE item in pListaPromocionVolumenlDetalle)
                     {
                         //Insertamos el detalle de la solicitud de producto
-                        item.IdPromocionTemporal = IdPromocionTemporal;
-                        PromocionTemporalDetalle.Inserta(item);
+                        item.IdPromocionVolumen = IdPromocionVolumen;
+                        PromocionVolumenlDetalle.Inserta(item);
                     }
 
                     if (pItem.FlagWeb)
                     {
-                        PromocionTemporal.ActualizaVistaWeb();
+                        PromocionVolumen.ActualizaVistaWeb();
                     }
 
                     ts.Complete();
@@ -64,6 +64,141 @@ namespace ErpPanorama.BusinessLogic
 
 
         }
+
+        public void Insertadt(PromocionVolumenDetalleBE pItem)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionTemporalDetalle = new PromocionVolumenDetalleDL();
+                PromocionTemporalDetalle.Inserta(pItem);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+
+        public void Actualiza(PromocionVolumenBE pItem, List<PromocionVolumenDetalleBE> pListaPromocionVolumenDetalle)
+        {
+            try
+            {
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    PromocionVolumenDL PromocionVolumen = new PromocionVolumenDL();
+                    PromocionVolumenDetalleDL PromocionVolumenDetalle = new PromocionVolumenDetalleDL();
+
+                    foreach (PromocionVolumenDetalleBE item in pListaPromocionVolumenDetalle)
+                    {
+                        if (item.TipoOper == Convert.ToInt32(Operacion.Nuevo)) //Nuevo
+                        {
+                            //Insertamos el detalle de la solicitud de producto
+                            item.IdPromocionVolumen = pItem.IdPromocionVolumen;
+                            PromocionVolumenDetalle.Inserta(item);
+                        }
+                        else if (item.TipoOper == Convert.ToInt32(Operacion.Modificar))
+                        {
+                            PromocionVolumenDetalle.Actualiza(item);
+                        }
+                        //else
+                        //{
+                        //    //Actualizamos el detalle de la solicitud de producto
+                        //    PromocionTemporalDetalle.Actualiza(item);
+                        //}
+                    }
+
+                    PromocionVolumen.Actualiza(pItem);
+
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+
+        public List<PromocionVolumenDetalleBE> ListaTodosActivo(int IdPromocionVolumen)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionTemporalDetalle = new PromocionVolumenDetalleDL();
+                return PromocionTemporalDetalle.ListaTodosActivo(IdPromocionVolumen);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+        public void Actualizadt(PromocionVolumenDetalleBE pItem)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionTemporalDetalle = new PromocionVolumenDetalleDL();
+                PromocionTemporalDetalle.Actualiza(pItem);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+
+        public void Elimina(PromocionVolumenBE pItem)
+        {
+            try
+            {
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    PromocionVolumenDL PromocionVolumen = new PromocionVolumenDL();
+                    PromocionVolumenDetalleDL PromocionVolumenDetalle = new PromocionVolumenDetalleDL();
+
+                    PromocionVolumenDetalleBE ojbE_PromocionTemporalDetalle = new PromocionVolumenDetalleBE();
+                    ojbE_PromocionTemporalDetalle.IdPromocionVolumen = pItem.IdPromocionVolumen;
+                    ojbE_PromocionTemporalDetalle.IdEmpresa = pItem.IdEmpresa;
+                    ojbE_PromocionTemporalDetalle.Maquina = pItem.Maquina;
+                    ojbE_PromocionTemporalDetalle.Usuario = pItem.Usuario;
+
+                    PromocionVolumenDetalle.EliminaTodo(ojbE_PromocionTemporalDetalle);
+
+                    PromocionVolumen.Elimina(pItem);
+
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+
+        public void Eliminadt(PromocionVolumenDetalleBE pItem)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionVolumenDetalle = new PromocionVolumenDetalleDL();
+                PromocionVolumenDetalle.Elimina(pItem);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+        public void EliminaTodo(PromocionVolumenDetalleBE pItem)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionVolumenDetalle = new PromocionVolumenDetalleDL();
+                PromocionVolumenDetalle.EliminaTodo(pItem);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+        public PromocionVolumenDetalleBE Selecciona(int IdEmpresa, int IdTipoCliente, int IdFormaPago, int IdTienda, int IdTipoVenta, int IdProducto, bool TraerIdTemDet = false)
+        {
+            try
+            {
+                PromocionVolumenDetalleDL PromocionVolumenDetalle = new PromocionVolumenDetalleDL();
+                return PromocionVolumenDetalle.Selecciona(IdEmpresa, IdTipoCliente, IdFormaPago, IdTienda, IdTipoVenta, IdProducto, TraerIdTemDet);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+
 
     }
 }
